0681929268199554902c145df463122239ee1dba
parkhr <vlhong94@gmail.com>
2020-12-28 23:02:06 +0900

    오늘 확진자 조회 응답 json변경

--- app.js
+++ app.js
@@ -17,43 +17,19 @@ app.post('/infect/today', (req, res) => {
     let today = new Date().toISOString().split('T')[0];
     InfectOnedaySchema.find({create_date: {$regex: '.*' + today + '.*'}}).then((result) => {
         if(result.length > 0){
-            let data = `{
-                "version": "2.0",
-                "template": {
-                  "outputs": [
+            let data = {
+                version: "2.0",
+                template: {
+                  outputs: [
                     {
-                      "basicCard": {
-                        "title": "보물상자",
-                        "description": "보물상자 안에는 뭐가 있을까",
-                        "thumbnail": {
-                          "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg"
-                        },
-                        "profile": {
-                          "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4BJ9LU4Ikr_EvZLmijfcjzQKMRCJ2bO3A8SVKNuQ78zu2KOqM",
-                          "nickname": "보물상자"
-                        },
-                        "social": {
-                          "like": 1238,
-                          "comment": 8,
-                          "share": 780
-                        },
-                        "buttons": [
-                          {
-                            "action": "message",
-                            "label": "열어보기",
-                            "messageText": "짜잔! 우리가 찾던 보물입니다"
-                          },
-                          {
-                            "action":  "webLink",
-                            "label": "구경하기",
-                            "webLinkUrl": "https://e.kakao.com/t/hello-ryan"
-                          }
-                        ]
+                      basicCard: {
+                        title: "보물상자",
+                        description: "보물상자 안에는 뭐가 있을까"
                       }
                     }
                   ]
                 }
-              }`
+              }
             // return res.json(result);
             return res.json(data);
         }
